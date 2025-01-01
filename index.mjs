c52fb4de19df8f4ad764f7ca7f8250bc2ec451ae
Subhamk2004 <subhamk1978@gmail.com>
2025-01-01 16:44:30 +0530

    changes

--- index.mjs
+++ index.mjs
@@ -22,6 +22,7 @@ const allowedOrigins = [
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:5175',
+    'https://performance-tracker-seven.vercel.app'
 ];
 
 const corsOptions = {
